62944436041c5b21f4dbe31402cd48ce3a4d1d30
emilio barrera <barrera_emilio@hotmail.com>
2019-12-21 20:34:10 +0000

    Factorial Android

---
+++ Factorial.Core/App.cs
@@ -0,0 +1,25 @@
+﻿using Factorial.Core.ViewModels;
+using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factorial.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<FactorialViewModel>();
+
+            base.Initialize();
+        }
+    }
+}
